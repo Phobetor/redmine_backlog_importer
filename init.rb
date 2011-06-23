0ed12f112f88a9fcd5cba52251574bafcb1c1b20
Phobetor <andreas.kintzinger@kwick.de>
2011-06-23 18:29:41 +0200

    project renamed (2)

--- init.rb
+++ init.rb
@@ -1,12 +1,12 @@
 require 'redmine'
 
-Redmine::Plugin.register :redmine_google_docs_backlog_importer do
-  name 'Redmine Google Docs Backlog Importer'
+Redmine::Plugin.register :redmine_backlog_importer do
+  name 'Redmine Backlog Importer'
   author 'Phobetor'
-  description 'Plugin to import copied content of a Google Docs table as issues into Redmine.'
+  description 'Plugin to import tab delimited backlogs (e. g. copied content of a Google Docs spreadsheet) into Redmine.'
   version '0.0.1'
-  url 'https://github.com/phobetor/RedmineGoogleDocsBacklogImporter'
+  url 'https://github.com/phobetor/RedmineBacklogImporter'
   author_url 'https://github.com/phobetor'
-  permission :google_docs_backlog_importer, {:google_docs_backlog_importer => [:index, :import]}, :public => true
-  menu :project_menu, :rgdb_importer, { :controller => 'google_docs_backlog_importer', :action => 'index' }, :caption => 'ImportIssues'
+  permission :backlog_importer, {:backlog_importer => [:index, :import]}, :public => true
+  menu :project_menu, :rgdb_importer, { :controller => 'backlog_importer', :action => 'index' }, :caption => 'ImportIssues'
 end
